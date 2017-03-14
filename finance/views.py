e88d2524841399772eba5b560d30d8ccd4993e33
balalay12 <balalay12@yandex.ru>
2017-03-14 10:49:49 +0500

    all forms to new folder

--- finance/views.py
+++ finance/views.py
@@ -8,7 +8,7 @@ from . import forms, models
 
 
 class UserLogin(FormView):
-    template_name = 'login.jinja2'
+    template_name = 'forms/login.jinja2'
     form_class = AuthenticationForm
     success_url = '/'
 
@@ -19,7 +19,7 @@ class UserLogin(FormView):
 
 
 class UserRegistration(FormView):
-    template_name = 'reg.jinja2'
+    template_name = 'forms/reg.jinja2'
     form_class = UserCreationForm
     success_url = '/login/'
 
@@ -66,7 +66,7 @@ class AccountsCreate(CreateView):
 class AccountUpdate(UpdateView):
     model = models.Accounts
     fields = ['name', 'score']
-    template_name = 'accounts_update_form.jinja2'
+    template_name = 'forms/accounts_update_form.jinja2'
     success_url = '/'
 
 
@@ -77,7 +77,7 @@ class AccountDelete(DeleteView):
 
 class BalanceCreate(CreateView):
     model = models.Balance
-    template_name = 'balance_new_form.jinja2'
+    template_name = 'forms/balance_new_form.jinja2'
     success_url = '/'
     form_class = forms.BalanceForm
 
